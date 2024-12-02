8856425b45505b8e27ac8997ca45a1b144f09a55
Dylan Hansen Wijaya <dylan.hansen096@gmail.com>
2024-12-02 22:49:38 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function navigate(page) {
+    alert(`Navigasi ke halaman: ${page}`);
+    // Anda dapat mengganti alert dengan navigasi nyata.
+    // Contoh: window.location.href = `${page}.html`;
+  }
+  
