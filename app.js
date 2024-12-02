7b3192188eaac7caca6a2e6bc4da6b3b85a042f4
Dylan Hansen Wijaya <dylan.hansen096@gmail.com>
2024-12-02 22:45:22 +0700

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-function navigate(page) {
-    alert(`Navigasi ke halaman: ${page}`);
-    // Anda dapat mengganti alert dengan navigasi nyata.
-    // Contoh: window.location.href = `${page}.html`;
-  }
-  
